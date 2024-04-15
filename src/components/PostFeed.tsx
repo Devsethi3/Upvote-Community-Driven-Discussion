8bfb60ff323c8620ce47d22e4858c7dba5bd4165
Devsethi3 <devsethi4522@gmail.com>
2024-04-15 14:47:31 +0530

    implementing infinite scrolling in post feed

--- src/components/PostFeed.tsx
+++ src/components/PostFeed.tsx
@@ -1,12 +1,49 @@
-import React from "react";
+"use client";
+
+import { INFINITE_SCROLLING_PAGINATION_RESULTS } from "@/config/config";
+import { ExtendedPost } from "@/types/db";
+import { useIntersection } from "@mantine/hooks";
+import { useInfiniteQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { Loader2 } from "lucide-react";
+import { useEffect, useRef } from "react";
+import { useSession } from "next-auth/react";
 
 interface PostFeedProps {
+  initialPosts: ExtendedPost[];
+  subredditName?: string;
 }
 
-const PostFeed:React.FC<PostFeedProps> = () => {
+const PostFeed: React.FC<PostFeedProps> = ({ initialPosts, subredditName }) => {
+  const lastPostRef = useRef<HTMLElement>(null);
+
+  const { ref, entry } = useIntersection({
+    root: lastPostRef.current,
+    threshold: 1,
+  });
+
+  // const { data, fetchNextPage, isFetchingNextPage } = useInfiniteQuery(
+  //   ["infinite-query"],
+  //   async ({ pageParam = 1 }) => {
+  //     const query =
+  //       `/api/posts?limit=${INFINITE_SCROLLING_PAGINATION_RESULTS}&page=${pageParam}` +
+  //       (!!subredditName ? `&subredditName=${subredditName}` : "");
+
+  //     const { data } = await axios.get(query);
+  //     return data as ExtendedPost[];
+  //   },
+
+  //   {
+  //     getNextPageParam: (_, pages) => {
+  //       return pages.length + 1;
+  //     },
+  //     initialData: { pages: [initialPosts], pageParams: [1] },
+  //   }
+  // );
+
   return (
     <>
-      <div>PostFeed</div>
+      <ul className="flex flex-col col-span-2 space-y-2"></ul>
     </>
   );
 };
