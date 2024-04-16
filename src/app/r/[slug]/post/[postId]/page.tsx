2d864dc937e7e475c4653af2a6a0546ed24eb4c3
Devsethi3 <devsethi4522@gmail.com>
2024-04-16 21:35:39 +0530

    creating single post page

--- src/app/r/[slug]/post/[postId]/page.tsx
+++ src/app/r/[slug]/post/[postId]/page.tsx
@@ -1,8 +1,44 @@
-interface PostPageProps {}
-const PostPage: React.FC<PostPageProps> = () => {
+import { db } from "@/lib/db";
+import { redis } from "@/lib/redis";
+import { CachedPost } from "@/types/redis";
+import { Post, User, Vote } from "@prisma/client";
+import { notFound } from "next/navigation";
+
+interface PostPageProps {
+  params: {
+    postId: string;
+  };
+}
+
+export const dynamic = "force-dynamic";
+export const fetchCache = "force-no-store";
+
+const PostPage = async ({ params }: PostPageProps) => {
+  const cachedPost = (await redis.hgetall(
+    `post:${params.postId}`
+  )) as CachedPost;
+
+  let post: (Post & { votes: Vote[]; author: User }) | null = null;
+
+  if (!cachedPost) {
+    post = await db.post.findFirst({
+      where: {
+        id: params.postId,
+      },
+      include: {
+        votes: true,
+        author: true,
+      },
+    });
+  }
+
+  if (!post && !cachedPost) return notFound();
+
   return (
     <>
-      <div>PostPage</div>
+      <div className="h-full flex flex-col sm:flex-row items-center sm:items-start justify-between">
+        <div></div>
+      </div>
     </>
   );
 };
