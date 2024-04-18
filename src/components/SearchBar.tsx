bad3133c84641b03de3e1e0490aea7d13d5798f1
Devsethi3 <devsethi4522@gmail.com>
2024-04-18 19:13:51 +0530

    search functionality

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,17 +1,38 @@
 "use client";
 
-import React, { useState } from "react";
-import { Command, CommandInput } from "./ui/command";
+import { Prisma, Subreddit } from "@prisma/client";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
-import { Prisma, Subreddit } from "@prisma/client";
-import { CommandEmpty, CommandGroup, CommandItem, CommandList } from "cmdk";
-import { Users } from "lucide-react";
-import { useRouter } from "next/navigation";
+import debounce from "lodash.debounce";
+import { usePathname, useRouter } from "next/navigation";
+import { FC, useCallback, useEffect, useRef, useState } from "react";
 
-const SearchBar = () => {
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "@/components/ui/command";
+import { Users } from "lucide-react";
+
+interface SearchBarProps {}
+
+const SearchBar: FC<SearchBarProps> = ({}) => {
   const [input, setInput] = useState<string>("");
+  const pathname = usePathname();
+  const commandRef = useRef<HTMLDivElement>(null);
   const router = useRouter();
+
+  const request = debounce(async () => {
+    refetch();
+  }, 300);
+
+  const debounceRequest = useCallback(() => {
+    request();
+
+  }, []);
 
   const {
     isFetching,
@@ -30,42 +51,48 @@ const SearchBar = () => {
     enabled: false,
   });
 
-  return (
-    <div>
-      <Command className="relative rounded-md border z-50 overflow-visible">
-        <CommandInput
-          value={input}
-          onValueChange={(text) => {
-            setInput(text);
-          }}
-          className="outline-none border-none focus:border-none focus-within:outline-none ring-0"
-          placeholder="Search Communities..."
-        />
+  useEffect(() => {
+    setInput("");
+  }, [pathname]);
 
-        {input.length > 0 && (
-          <CommandList className="absolute bg-slate-50 top-full inset-x-0 shadow rounded-b-md">
-            {isFetched && <CommandEmpty>No Results Found.</CommandEmpty>}
-            {(queryResults?.length ?? 0) > 0 ? (
-              <CommandGroup heading="Communities">
-                {queryResults?.map((subreddit) => (
-                  <CommandItem
-                    onSelect={(e) => {
-                      router.push(`/r/${e}`);
-                      router.refresh();
-                    }}
-                    key={subreddit.id}
-                    value={subreddit.name}
-                  >
-                    <Users className="mr-2 h-4 w-4" />
-                    <a href={`/r/${subreddit.name}`}>r/{subreddit.name}</a>
-                  </CommandItem>
-                ))}
-              </CommandGroup>
-            ) : null}
-          </CommandList>
-        )}
-      </Command>
-    </div>
+  return (
+    <Command
+      ref={commandRef}
+      className="relative rounded-lg border max-w-lg z-50 overflow-visible"
+    >
+      <CommandInput
+        onValueChange={(text) => {
+          setInput(text);
+          debounceRequest();
+        }}
+        value={input}
+        className="outline-none border-none focus:border-none focus:outline-none ring-0"
+        placeholder="Search communities..."
+      />
+
+      {input.length > 0 && (
+        <CommandList className="absolute bg-white top-full inset-x-0 shadow rounded-b-md">
+          {isFetched && <CommandEmpty>No results found.</CommandEmpty>}
+          {(queryResults?.length ?? 0) > 0 ? (
+            <CommandGroup heading="Communities">
+              {queryResults?.map((subreddit) => (
+                <CommandItem
+                  onSelect={(e) => {
+                    router.push(`/r/${e}`);
+                    router.refresh();
+                  }}
+                  key={subreddit.id}
+                  value={subreddit.name}
+                >
+                  <Users className="mr-2 h-4 w-4" />
+                  <a href={`/r/${subreddit.name}`}>r/{subreddit.name}</a>
+                </CommandItem>
+              ))}
+            </CommandGroup>
+          ) : null}
+        </CommandList>
+      )}
+    </Command>
   );
 };
 
