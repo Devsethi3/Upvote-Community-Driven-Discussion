ee05e7dff601871fa0d51662f179a142c6e6f883
Dev Prasad Sethi <147725410+Devsethi3@users.noreply.github.com>
2024-05-06 22:49:49 +0530

    Update route.ts

--- src/app/api/uploadthing/route.ts
+++ src/app/api/uploadthing/route.ts
@@ -2,7 +2,7 @@ import { createNextRouteHandler } from 'uploadthing/next'
 
 import { ourFileRouter } from './core'
 
-// Export routes for Next
+// Export routes for 
 export const { GET, POST } = createNextRouteHandler({
   router: ourFileRouter,
 })
